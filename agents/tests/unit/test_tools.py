d269294a367af46dde860001db87120130a984da
Nikola Forró <nforro@redhat.com>
2025-10-24 08:24:40 +0200

    se "higher stream" rather than Y-Stream
    For EUS branches the base release number comes from the higher Z-Stream,
    not Y-Stream. Even though the code works with EUS branches just fine,
    variables and functions still reference Y-Stream which is not accurate.
    
    Signed-off-by: Nikola Forró <nforro@redhat.com>

--- agents/tests/unit/test_tools.py
+++ agents/tests/unit/test_tools.py
@@ -137,22 +137,17 @@ def autorelease_spec(tmp_path):
     [False, True],
 )
 @pytest.mark.parametrize(
-    "dist_git_branch, ystream_dist",
-    [
-        ("c9s", ".el9"),
-        ("c10s", ".el10"),
-        ("rhel-9.6.0", ".el9"),
-        ("rhel-10.0", ".el10"),
-    ],
+    "dist_git_branch",
+    ["c9s", "c10s", "rhel-9.6.0", "rhel-10.0"],
 )
 @pytest.mark.asyncio
-async def test_update_release(rebase, dist_git_branch, ystream_dist, minimal_spec, autorelease_spec):
+async def test_update_release(rebase, dist_git_branch, minimal_spec, autorelease_spec):
     package = "test"
 
-    async def _get_latest_ystream_build(*_, **__):
-        return EVR(version="0.1", release="2" + ystream_dist)
+    async def _get_latest_higher_stream_build(*_, **__):
+        return EVR(version="0.1", release="2.elX")
 
-    flexmock(UpdateReleaseTool).should_receive("_get_latest_ystream_build").replace_with(_get_latest_ystream_build)
+    flexmock(UpdateReleaseTool).should_receive("_get_latest_higher_stream_build").replace_with(_get_latest_higher_stream_build)
 
     tool = UpdateReleaseTool()
 

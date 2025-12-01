1e1eae011eb97bf9590a41a663d11519a56d4519
Tomas Korbar <tkorbar@redhat.com>
2025-12-01 09:38:34 +0100

    Add type hints where appropriate

--- agents/tests/e2e/conftest.py
+++ agents/tests/e2e/conftest.py
@@ -1,7 +1,13 @@
+from typing import Generator
+
+
 import pytest
 
+
 @pytest.hookimpl(wrapper=True)
-def pytest_terminal_summary(terminalreporter: pytest.TerminalReporter, exitstatus, config: pytest.Config):
+def pytest_terminal_summary(
+    terminalreporter: pytest.TerminalReporter, exitstatus, config: pytest.Config
+) -> Generator:
     yield
     metrics = config.stash.get("metrics", None)
 

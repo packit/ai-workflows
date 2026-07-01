b15791d49290ca1e17fc731514ffa6c30279f0ac
Matej Focko <mfocko@packit.dev>
2026-07-01 17:28:02 +0200

    test: report to tmt as custom results
    Reasoning:
    1. Tests are run concurrently ~> no reasonable logs from pytest
    2. We scrape tracing server for debugging purposes ~> additional artifact
    3. Each test case is quite complex as it runs an LLM workflow, producing
       traces and also some metrics are collected ~> there's a lot of
       attributes that can be associated with each test
    
    Benefits:
    1. Each test case is reported as separate subtest on tmt' side, therefore
       it is easy to bundle all the artifacts, collected metrics and test
       result together.
    2. It is possible to tell at first sight which test cases failed, as they
       are reported by Testing Farm view separately and each test case is
       identified by the Jira ticket.
    3. Collected metrics (displayed in terminal via pytest as a table) are
       presented as notes:
       - agent
       - tool calls
       - prompt tokens
       - completion tokens
    4. Since test cases are run before the actual assertions happen, we can
       also propagate the duration of tests this way. pytest assertions do
       not capture the actual duration of the test case.
    
    Signed-off-by: Matej Focko <mfocko@packit.dev>

--- plans/run-triage.sh
+++ plans/run-triage.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+touch ${TMT_TEST_DATA}/results.yaml
+
 # Run the tests
 make run-triage-agent-e2e-tests
 
@@ -7,9 +9,6 @@ make run-triage-agent-e2e-tests
 JIRAS=$(make list-triage-agent-e2e-tests | sed -rn 's/.*\[(RHEL-.+)\].*/\1/gp')
 
 for TICKET in $JIRAS; do
-    curl -H 'Accept: application/json' http://localhost:8082/traces/$TICKET > $TICKET.json
-    tmt-file-submit -l $TICKET.json
-
-    curl -H 'Accept: text/html' http://localhost:8082/traces/$TICKET > $TICKET.html
-    tmt-file-submit -l $TICKET.html
+    curl -H 'Accept: application/json' http://localhost:8082/traces/$TICKET > ${TMT_TEST_DATA}/${TICKET}.json
+    curl -H 'Accept: text/html' http://localhost:8082/traces/$TICKET > ${TMT_TEST_DATA}/${TICKET}.html
 done;
